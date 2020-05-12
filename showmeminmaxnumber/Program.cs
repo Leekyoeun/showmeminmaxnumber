2f2786c4ceba3758ce7b18afbf664fb1c1927e4b
s2018 <s2018s35@e-mirim.hs.kr>
2020-05-12 10:37:37 +0900

    초기 커밋.

---
+++ showmeminmaxnumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace showmeminmaxnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            int first = 0;
+            int second=0;
+            int thrid=0;
+            int four=0;
+            int five=0;
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("숫자를 입력하세요");
+                Console.WriteLine();
+            }
+            Console.WriteLine(Math.Max(first, second, thrid, four, five));
+            Console.WriteLine(Math.Min(first, second, thrid, four, five));
+
+        }
+    }
+}
